52fe0a2e6110631c90265a33c3977f3077c13816
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-04-22 21:32:55 +0545

    register user, search functionality

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,9 @@ from . import views as base_views
 urlpatterns = [
     path('login/', base_views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', base_views.RegisterPage.as_view(), name='register'),
+
+
     path('', base_views.TaskList.as_view(), name='task-list'),
     path('task/<int:pk>/', base_views.TaskDetail.as_view(), name='task'),
     path('task-create/', base_views.TaskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.forms.forms import Form
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from base.models import Task
 # Create your views here.
@@ -17,6 +20,23 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('task-list')
 
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task-list')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name="task_list"
@@ -25,6 +45,11 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['task_list'] = context['task_list'].filter(user=self.request.user)
         context['count'] = context['task_list'].filter(complete_status=False)
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['task_list'] = context['task_list'].filter(title__icontains=search_input)
+            context['search_input'] = search_input
         return context
 
 
