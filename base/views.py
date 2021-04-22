01624461002b2966dd48b7746de0fc5582ec4467
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-04-22 21:08:08 +0545

    user restriction on pages, user specific data

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from base.models import Task
 # Create your views here.
@@ -16,26 +17,37 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('task-list')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name="task_list"
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task_list'] = context['task_list'].filter(user=self.request.user)
+        context['count'] = context['task_list'].filter(complete_status=False)
+        return context
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name='task'
     template_name='base/task_detail.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete_status']
     success_url = reverse_lazy('task-list')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete_status']
     success_url = reverse_lazy('task-list')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name='task'
     success_url = reverse_lazy('task-list')
--- todo_list_app/settings.py
+++ todo_list_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'base.apps.BaseConfig',
 ]
 
@@ -115,6 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
